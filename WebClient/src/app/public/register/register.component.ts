9342f8a60477812b8bbc3fa4a958925676c432fc
Sava Ivanov <sava.ivanov.9@gmail.com>
2020-03-22 21:16:01 +0200

    Register component implemented

---
+++ WebClient/src/app/public/register/register.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+
+import {
+  PasswordRegex,
+  UserNameRegex
+} from '../../constants/regexs';
+
+import { RegisterRequest } from 'src/app/models/register-request.model';
+import { RegisterResponse } from 'src/app/models/register-response.model';
+
+import { UiNotificationService } from 'src/app/services/ui-notifications.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  registerData: RegisterRequest = new RegisterRequest();
+
+  passwordFormControl = new FormControl(this.registerData.password, [
+    Validators.required,
+    Validators.pattern(PasswordRegex),
+    Validators.maxLength(30),
+    Validators.minLength(5)
+  ]);
+
+  userNameFormControl = new FormControl(this.registerData.name, [
+    Validators.required,
+    Validators.pattern(UserNameRegex),
+    Validators.maxLength(20),
+    Validators.minLength(1)
+  ]);
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private uiNotificationService: UiNotificationService
+  ) { }
+
+  ngOnInit() { }
+
+  register() {
+    this.updateModel();
+
+    if (this.validateInputData()) {
+      this.authService.register(
+        this.registerData,
+        (result: RegisterResponse) => {
+          if (result.userId) {
+            this.uiNotificationService.showSuccess('Successful registration');
+            this.router.navigate(['rooms']);
+          } else {
+            this.uiNotificationService.showSuccess('Unsuccessful registration');
+          }
+        }
+      );
+    } else {
+      this.uiNotificationService.showWarn('Invalid input');
+    }
+  }
+
+  updateModel() {
+    this.registerData.name = this.userNameFormControl.value;
+    this.registerData.password = this.passwordFormControl.value;
+  }
+
+  validateInputData(): boolean {
+    // const hasError = this.ValidatePassword() || this.ValidateUserName();
+    const hasError = this.ValidateUserName();
+    return hasError === false;
+  }
+
+  private ValidatePassword() {
+    return (
+      this.passwordFormControl.hasError('required') ||
+      this.passwordFormControl.hasError('pattern') ||
+      this.passwordFormControl.hasError('maxlength') ||
+      this.passwordFormControl.hasError('minlength')
+    );
+  }
+
+  private ValidateUserName() {
+    return (
+      this.userNameFormControl.hasError('required') ||
+      this.userNameFormControl.hasError('pattern') ||
+      this.userNameFormControl.hasError('maxlength') ||
+      this.userNameFormControl.hasError('minlength')
+    );
+  }
+}
