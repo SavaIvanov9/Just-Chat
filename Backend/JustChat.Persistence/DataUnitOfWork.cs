d0330f4b184252aae311efcd2a3d3a063a8635b1
Sava Ivanov <sava.ivanov.9@gmail.com>
2020-03-22 12:59:52 +0200

    Data unit of work implemented

---
+++ Backend/JustChat.Persistence/DataUnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using JustChat.Application.Interfaces;
+using JustChat.Domain.Models.Rooms;
+using JustChat.Domain.Models.Users;
+using JustChat.Persistence.Commands;
+
+namespace JustChat.Persistence
+{
+    internal class DataUnitOfWork : IDataUnitOfWork
+    {
+        private readonly CommandDbContext _commandDbContext;
+
+        public DataUnitOfWork(
+            CommandDbContext commandDbContext,
+            IMutatableRepository<User> users,
+            IMutatableRepository<Room> rooms)
+        {
+            _commandDbContext = commandDbContext;
+            Users = users;
+            Rooms = rooms;
+        }
+
+        public IMutatableRepository<User> Users { get; }
+
+        public IMutatableRepository<Room> Rooms { get; }
+
+        public bool HasActiveTransaction => _commandDbContext.HasActiveTransaction;
+
+        public async Task BeginTransactionAsync()
+        {
+            await _commandDbContext.BeginTransactionAsync();
+        }
+
+        public async Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            await _commandDbContext.CommitAsync();
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            await _commandDbContext.RollbackTransactionAsync();
+        }
+    }
+}
---
+++ Backend/JustChat.Persistence/DependencyRegistrations.cs
@@ -0,0 +1,16 @@
+﻿using JustChat.Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JustChat.Persistence
+{
+    public static class DependencyRegistrations
+    {
+        public static IServiceCollection RegisterPersistenceDepenencies(this IServiceCollection services, string commandDbConnectionString)
+        {
+            services.RegisterPersistanceCommandsDependencies(commandDbConnectionString);
+            services.AddTransient<IDataUnitOfWork, DataUnitOfWork>();
+
+            return services;
+        }
+    }
+}
