7d7437e39d0f0aef831caf488a5a4990c7b4cf6e
Sava Ivanov <sava.ivanov.9@gmail.com>
2020-03-22 21:13:04 +0200

    Configurations added

---
+++ WebClient/src/app/constants/configuration.ts
@@ -0,0 +1,4 @@
+export const ApiUrl = 'http://localhost:9468/api/';
+export const RegisterUrl = `${ApiUrl}Register`;
+export const MessageHubUrl = `${ApiUrl}MessageHub`;
+export const MessageHubReconnectInterval = 5000;
---
+++ WebClient/src/app/constants/regexs.ts
@@ -0,0 +1,2 @@
+export const PasswordRegex = '^[a-zA-Z0-9!@#$%^&*()_+]*$';
+export const UserNameRegex = '^[a-zA-Z0-9-.]*$';
