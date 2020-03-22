9546d1c8111b10d08bad7c3a02c1705e53034050
Sava Ivanov <sava.ivanov.9@gmail.com>
2020-03-22 21:16:24 +0200

    Url auth gurad added

---
+++ WebClient/src/app/guards/authentication-guard.ts
@@ -0,0 +1,21 @@
+import { CanActivate, UrlTree, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { AuthenticationService } from './../services/authentication.service';
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router) { }
+
+  canActivate(): Observable<boolean | UrlTree> {
+    if (this.authService.IsAuthenticated) {
+      return of(true);
+    }
+
+    this.router.navigate(['/home']);
+    return of(false);
+  }
+}
