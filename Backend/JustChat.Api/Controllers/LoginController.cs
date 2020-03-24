0377d6091992d7e7d3edc0611f6e6c2a955880a3
Sava Ivanov <sava.ivanov.9@gmail.com>
2020-03-24 08:25:39 +0200

    Login controller implemented

---
+++ Backend/JustChat.Api/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using JustChat.Api.Models.Login;
+using JustChat.Application.Features.Commands.LogInUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustChat.Api.Controllers
+{
+    public class LoginController : BaseController
+    {
+        public LoginController(IMediator mediator)
+            : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<LogInResponse>> LogIn([FromBody]LogInRequest request)
+        {
+            var command = new LogInUserCommand
+            {
+                UserName = request.UserName,
+                Password = request.Password
+            };
+
+            var token = await Mediator.Send(command);
+
+            var response = new LogInResponse
+            {
+                Token = token.Value,
+                UserId = token.UserId
+            };
+
+            return this.Ok(response);
+        }
+    }
+}
---
+++ Backend/JustChat.Api/Models/Login/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace JustChat.Api.Models.Login
+{
+    public class LogInResponse
+    {
+        public string Token { get; set; }
+
+        public long UserId { get; set; }
+    }
+}
