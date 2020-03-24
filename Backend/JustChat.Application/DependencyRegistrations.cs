35b2c38735706c012994e45aef7f2545903811f8
Sava Ivanov <sava.ivanov.9@gmail.com>
2020-03-24 08:23:00 +0200

    Application dependency registrations added

---
+++ Backend/JustChat.Application/DependencyRegistrations.cs
@@ -0,0 +1,25 @@
+﻿using JustChat.Application.Extensions;
+using JustChat.Application.Interfaces.Services;
+using JustChat.Application.Models;
+using JustChat.Application.Services;
+using JustChat.Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JustChat.Persistence
+{
+    public static class DependencyRegistrations
+    {
+        public static IServiceCollection RegisterApplicationDepenencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            var tokenConfig = configuration.Bind<TokenConfiguration>(nameof(TokenConfiguration));
+            services.AddSingleton(tokenConfig);
+            
+            services.AddTransient<ITokenService, TokenService>();
+            services.AddTransient<IEncryptionService, EncryptionService>();
+            services.AddTransient<IHashingService, HashingService>();
+
+            return services;
+        }
+    }
+}
---
+++ Backend/JustChat.Application/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace JustChat.Application.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static T Bind<T>(this IConfiguration configuration, string key)
+            where T : class
+        {
+            return configuration.GetSection(key).Get<T>(options => options.BindNonPublicProperties = true);
+        }
+    }
+}
